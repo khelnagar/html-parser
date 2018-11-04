36a3c18fb03be78bbea5ecfd5e4f13448e995695
KhElnagar <khaled.elnagar@relianceegypt.com>
2018-11-04 17:33:24 +0200

    accounted for h4, h5, h6 tags

--- hackathon.py
+++ hackathon.py
@@ -3,7 +3,7 @@
 from bs4 import BeautifulSoup
 import json
 
-file_name = 'example.html'
+file_name = 'inputs/5bd6c00d652cfa3480dff05398d0e4b6'
 
 
 def get_node_text(node):
@@ -15,7 +15,7 @@ def get_node_text(node):
 
 def next_p_texts(node, include_first=False):
     """Given a node, it returns a list of the insider texts of all next <p> and <ul>
-    tags at certain hierarchy level. The input node could be a <heading> or a <p> tag.
+    tags at same node level. The input node could be a <heading> or a <p> tag.
     """
 
     p_tags_texts = []
@@ -30,7 +30,7 @@ def next_p_texts(node, include_first=False):
             p_tags_texts.append(sib.get_text())
         elif sib.name == 'p':
             p_tags_texts.append(get_node_text(sib))
-        if sib.name in ['h2', 'h3']:
+        if sib.name in ['h2', 'h3', 'h4', 'h5', 'h6']:
             break
         else:
             continue
@@ -38,37 +38,51 @@ def next_p_texts(node, include_first=False):
     return p_tags_texts
 
 
-def next_h3_tags(node):
-    """Given an <h3> node, it returns a list of the adjacent <h3> siblings at same level."""
+def next_h_tags(node):
+    """Given an <h> node, it returns a list of the adjacent <h> siblings at same node level."""
 
-    h3_tags = []
+    # h_tags = []
 
-    for sib in node.next_siblings:
-        if sib.name == 'h3':
-            h3_tags.append(sib)
-        elif sib.name == 'h2':
-            break
-        else:
-            continue
+    # for sib in node.next_siblings:
+    #     if sib.name in ['h3', 'h4', 'h5', 'h6']:
+    #         h_tags.append(sib)
+    #     elif sib.name == 'h2':
+    #         break
+    #     else:
+    #         continue
 
-    return h3_tags
+    # return h_tags
+    print (node.name)
+    if not node:
+    	return []
+    else:
+    	return [node] + next_h_tags(node.next_sibling.next_sibling)
+    # else:
+    # 	return next_h_tags(node.next_sibling.next_sibling)
 
+
+# def recurMul(a, b):
+
+#     if b == 1:
+#         return a
+#     else:
+#         return a + recurMul(a, b-1)
 
 def get_subsections(node):
-    """Given an <h3> tag, it returns a list of the insider texts of all next <p> and <ul>
+    """Given an <h2> tag, it returns a list of the insider texts of all next <p> and <ul>
     tags at certain hierarchy level."""
     subsections = []
-    for h3 in next_h3_tags(node):
+    for h in next_h_tags(node):
         subsections.append({
-            "subsection": h3.get_text(),
-            "paragraphs": next_p_texts(h3)
+            "subsection": h.get_text(),
+            "paragraphs": next_p_texts(h)
         })
     return subsections
 
 
 def extract_headers(parent_node):
-    """This method take the parent node with class attribute "body searchable-content", and returns
-    the headers "table of contents" in a JSON format"""
+    """This method take the parent node with class attribute "body searchable-content", 
+    and returns the headers "table of contents" in a JSON format"""
 
     result = {}
 
@@ -108,7 +122,7 @@ try:
     soup = BeautifulSoup(contents, 'html.parser')
     parent_node = soup.find('div', class_="body searchable-content")
 
-    with open(file_name + '.body.json', 'w', encoding='utf8') as outfile:
+    with open(file_name + '.result.json', 'w', encoding='utf8') as outfile:
         json.dump(extract_headers(parent_node), outfile, ensure_ascii=False)
 
 except IOError as e:
